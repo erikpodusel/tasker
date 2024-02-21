928e4e6a5c7aacfb784b6fed6591495a54621ce3
erikpodusel <erik.podusel@gmail.com>
2024-02-21 11:48:41 +0100

    style: added Manrope font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./view/**/*.html",
-];
+export const content = ["./view/**/*.html"];
 export const theme = {
   extend: {
     colors: {
-      dark: '#151515'
-    }
+      dark: "#151515",
+    },
+    fontFamily: {
+      manrope: ["Manrope", "sans-serif"],
+    },
   },
 };
-export const plugins = [
-];
+export const plugins = [];
 export const corePlugins = { preFlight: true };
-
